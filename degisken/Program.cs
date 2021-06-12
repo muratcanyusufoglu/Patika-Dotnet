4f9b77145f0e336acbced1b6e5a9bdffae653a9b
muratcanyusufoglu <59195546+muratcanyusufoglu@users.noreply.github.com>
2021-06-12 19:35:45 +0300

    Degisken Ornekler

---
+++ degisken/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        string str10 ="20";
+
+        int a=12;
+
+        float ortalama=7.5f;
+
+        decimal sayi2=2155.32m;
+
+        int degisken=a+Convert.ToInt32(str10);
+
+        Console.WriteLine(degisken);
+
+        string datetime=DateTime.Now.ToString("dd.MM.yyyy");
+         Console.WriteLine(datetime);
+        
+        string datetime2=DateTime.Now.ToString("dd-MM-yyyy");
+         Console.WriteLine(datetime2);
+        
+        string hour=DateTime.Now.ToString("hh/mm");
+         Console.WriteLine(hour);       
+
+
+
+
+        
+
+
+
+    
+
+
+        }
+    }
+}
