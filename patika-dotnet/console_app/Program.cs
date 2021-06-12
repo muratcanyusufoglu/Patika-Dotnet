5a71d20dcf7999bf70231be34e10b242a2234b9d
muratcanyusufoglu <59195546+muratcanyusufoglu@users.noreply.github.com>
2021-06-12 17:56:22 +0300

    Console App

---
+++ patika-dotnet/console_app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz");
+            string isim=Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz");
+            string soyisim=Console.ReadLine();
+
+            Console.WriteLine("Merhaba benim adim "+isim+" "+soyisim );
+            
+        }
+    }
+}
