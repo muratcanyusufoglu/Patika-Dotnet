882fbfc80a826ced7fbbd2714879221743b67695
muratcanyusufoglu <59195546+muratcanyusufoglu@users.noreply.github.com>
2021-06-14 01:06:06 +0300

    Switch_case Ornek

---
+++ switch_case/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month=DateTime.Now.Month;
+
+            switch(month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kis Mevsimindesiniz....colddd");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Bahar Mevsimindesiniz....yesillendi ortalik");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz geldi....parmak arasi terlikler ciksinn");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Ve sonbahar....her yerde yaprak var ama her ye");
+                    break;
+
+                default:
+                    Console.WriteLine("Mevsimsizsiniz.....");
+                break;
+
+            }
+        }
+    }
+}
